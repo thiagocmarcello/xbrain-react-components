53c856bf587f145fc6bdd7c91f94dbc72ab69f44
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2018-02-03 04:21:06 -0200

    Fix link style when use XTypography

--- src/lib/components/Typography/index.js
+++ src/lib/components/Typography/index.js
@@ -1,26 +1,44 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Divider, Typography } from 'material-ui';
+import withStyles from 'material-ui/styles/withStyles';
 
+const styles = theme => ({
+  root: {
+    '&, &:hover': {
+      color: theme.palette.text.primary,
+    },
+  },
+  divider: {
+    display: 'block',
+    marginTop: 8,
+  },
+});
 class XTypography extends PureComponent {
   render() {
-    const { children, divider, ...rest } = this.props;
+    const {
+      children, divider, classes, className, ...rest
+    } = this.props;
+
     return (
-      <Typography {...rest}>
+      <Typography className={[classes.root, className].join(' ')} {...rest}>
         {children}
-        {divider && <Divider style={{ display: 'block', marginTop: 8 }} component="span" />}
+        {divider && <Divider className={classes.divider} component="span" />}
       </Typography>
     );
   }
 }
 
 XTypography.defaultProps = {
+  className: '',
   divider: false,
 };
 
 XTypography.propTypes = {
   children: PropTypes.node.isRequired,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
   divider: PropTypes.bool,
 };
 
-export default XTypography;
+export default withStyles(styles)(XTypography);
