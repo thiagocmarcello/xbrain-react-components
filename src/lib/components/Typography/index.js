d23ab0b6f5c27fe562d87cf44537dbb449a2dbde
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2018-02-08 00:28:08 -0200

    Add prop gutterTop in XTypography

--- src/lib/components/Typography/index.js
+++ src/lib/components/Typography/index.js
@@ -1,23 +1,47 @@
 import { Divider, Typography } from 'material-ui';
+import { withStyles } from 'material-ui/styles';
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { withStyles } from 'material-ui/styles';
 
-const styles = () => ({
-  divider: {
-    display: 'block',
-    marginTop: 8,
-  },
-});
+const styles = (theme) => {
+  const GUTTER_SIZE = theme.spacing.unit;
+  return {
+    divider: {
+      display: 'block',
+      marginTop: GUTTER_SIZE,
+    },
+    gutterTop1x: { marginTop: GUTTER_SIZE * 1 },
+    gutterTop2x: { marginTop: GUTTER_SIZE * 2 },
+    gutterTop3x: { marginTop: GUTTER_SIZE * 3 },
+    gutterBottom1x: { marginBottom: GUTTER_SIZE * 1 },
+    gutterBottom2x: { marginBottom: GUTTER_SIZE * 2 },
+    gutterBottom3x: { marginBottom: GUTTER_SIZE * 3 },
+  };
+};
 
 class XTypography extends PureComponent {
+  getGutterStyle = () => {
+    const { gutterBottom, gutterTop, classes } = this.props;
+
+    const gutterTopSize = typeof gutterTop === 'boolean' && gutterTop ? '1x' : gutterTop;
+
+    const gutterBottomSize =
+      typeof gutterBottom === 'boolean' && gutterBottom ? '1x' : gutterBottom;
+
+    if (gutterTop) {
+      return classes[`gutterTop${gutterTopSize}`];
+    }
+
+    return gutterBottom ? classes[`gutterBottom${gutterBottomSize}`] : '';
+  };
+
   render() {
     const {
-      children, divider, classes, ...rest
+      children, divider, classes, gutterBottom, gutterTop, className, ...rest
     } = this.props;
 
     return (
-      <Typography {...rest}>
+      <Typography className={`${this.getGutterStyle()} ${className}`} {...rest}>
         {children}
         {divider && <Divider className={classes.divider} component="span" />}
       </Typography>
@@ -26,13 +50,19 @@ class XTypography extends PureComponent {
 }
 
 XTypography.defaultProps = {
+  className: '',
   divider: false,
+  gutterBottom: false,
+  gutterTop: false,
 };
 
 XTypography.propTypes = {
   children: PropTypes.node.isRequired,
   classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
   divider: PropTypes.bool,
+  gutterBottom: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['1x', '2x', '3x'])]),
+  gutterTop: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['1x', '2x', '3x'])]),
 };
 
 export default withStyles(styles)(XTypography);
